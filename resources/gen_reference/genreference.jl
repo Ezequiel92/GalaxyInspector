a2e7cfb1843f371b65b491cc1ca6ca71e5697059
Ezequiel <elozano@df.uba.ar>
2022-07-19 10:26:47 +0200

    Cleaner artifact generation

--- resources/gen_reference/genreference.jl
+++ resources/gen_reference/genreference.jl
@@ -8,10 +8,10 @@ const GI = GalaxyInspector;
 using JLD2, Unitful, UnitfulAstro, CairoMakie, Tar, Pkg.Artifacts
 import GadgetIO as GIO
 
+# Filename of the generated artifact
 const NAME = "reference_results"
-
 # Path to the directory where the reference results will be saved
-const BASE_OUT_PATH = joinpath(@__DIR__, "../$NAME")
+const BASE_OUT_PATH = joinpath(@__DIR__, "../$NAME/")
 # Path to the directory containing the snapshot files used to generate the reference results
 const BASE_SRC_PATH = artifact"example_data"
 # Base name of the snapshot files, set in the GADGET variable `SnapshotFileBase`
@@ -33,17 +33,20 @@ include("reference_data_analysis.jl")
 include("reference_post_processing.jl")
 include("reference_pipelines.jl")
 
-# Compress results to be used as artifacts and delete files
-hash = create_artifact(dir -> cp(joinpath(@__DIR__, "../$NAME/"), dir, force=true))
+# Create and archive artifact
+hash = create_artifact(dir -> cp(BASE_OUT_PATH, dir, force=true))
 tarball_hash = archive_artifact(hash, joinpath(@__DIR__, "../$NAME.tar"))
+
+# Crate `Artifacts.toml` file
 bind_artifact!(
     joinpath(@__DIR__, "../../Artifacts.toml"),
     NAME,
     hash,
     download_info=[
-        ("https://github.com/Ezequiel92/GalaxyInspector/raw/main/resources/$NAME.tar", tarball_hash),
+        ("https://github.com/ezequiel92/GalaxyInspector/raw/main/resources/$NAME.tar", tarball_hash),
     ],
     force=true,
 )
 
+# Clean auxiliary files
 rm(BASE_OUT_PATH, recursive=true)
