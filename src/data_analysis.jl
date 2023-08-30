54f3e46e0ad33e3de6b12d007cb555ea9782eeb9
Ezequiel <elozano@df.uba.ar>
2023-08-30 16:05:33 -0300

    Doc correction

--- src/data_analysis.jl
+++ src/data_analysis.jl
@@ -266,9 +266,9 @@ Compute a profile for the Milky Way, compatible with the experimental data in Mo
       + `:molecular_area_density` -> Molecular hydrogen area mass density.
       + `:atomic_area_density`    -> Atomic hydrogen area mass density.
       + `:sfr_area_density`       -> Star formation rate area density, for the last `AGE_RESOLUTION_ρ`.
-      + `:O_stellar_abundance`    -> Stellar abundance of oxygen, as ``12 + \\log_{10}(O \\, / \\, H)``.
-      + `:N_stellar_abundance`    -> Stellar abundance of nitrogen, as ``12 + \\log_{10}(N \\, / \\, H)``.
-      + `:C_stellar_abundance`    -> Stellar abundance of carbon, as ``12 + \\log_{10}(C \\, / \\, H)``.
+      + `:O_stellar_abundance`    -> Stellar abundance of oxygen, as ``12 + \\log_{10}(\\mathrm{O \\, / \\, H})``.
+      + `:N_stellar_abundance`    -> Stellar abundance of nitrogen, as ``12 + \\log_{10}(\\mathrm{N \\, / \\, H})``.
+      + `:C_stellar_abundance`    -> Stellar abundance of carbon, as ``12 + \\log_{10}(\\mathrm{C \\, / \\, H})``.
 
 # Returns
 
